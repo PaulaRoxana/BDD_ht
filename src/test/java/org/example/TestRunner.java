0eae7479aa5da7394a5e2c536af227e807c8b7b6
PaulaRoxana <paula_girdea@yahoo.com>
2022-10-19 17:23:53 +0300

    initial commit

---
+++ src/test/java/org/example/TestRunner.java
@@ -0,0 +1,15 @@
+package org.example;
+
+//import io.cucumber.junit.CucumberOptions;
+import io.cucumber.testng.AbstractTestNGCucumberTests;
+import io.cucumber.testng.CucumberOptions;
+
+@CucumberOptions(
+        features = "src/test/resources/features",
+        glue = {"browsers/stepdefinitions"},
+        tags = "@SmokeTest",
+        plugin = {"pretty"}
+)
+public class TestRunner extends AbstractTestNGCucumberTests {
+
+}
